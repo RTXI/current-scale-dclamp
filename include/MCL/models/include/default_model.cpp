93027c4a58c228e4c40670525dffff633ae90d86
Ansel <aesthapus@gmail.com>
2016-02-26 17:18:31 -0500

    reformat code

--- include/MCL/models/include/default_model.cpp
+++ include/MCL/models/include/default_model.cpp
@@ -38,73 +38,87 @@ using namespace std;
 
 /*** default_model constructor ***/
 default_model::default_model(){
-    I_Inject = 0; // I_Inject must be initialized to 0 to prevent errors
+	I_Inject = 0; // I_Inject must be initialized to 0 to prevent errors
 
-    // DT and steps initalized for a RTXI thread rate of 10khz, and model rate of 100khz
-    DT = 0.01;
-    steps = 10;
+	// DT and steps initalized for a RTXI thread rate of 10khz, and model 
+	// rate of 100khz
+	DT = 0.01;
+	steps = 10;
 
-    // Create RTMath object
+	// Create RTMath object
 	RTmath = new RealTimeMath();
 }
 
 /*** default_model destructor ***/
 default_model::~default_model(){
-    delete RTmath;
+	delete RTmath;
 }
 
 /*** Set functions ***/
 void default_model::setDT(double x){
-    DT = x;
+	DT = x;
 }
 
 void default_model::setSteps(int x){
-    steps = x;
+	steps = x;
 }
+
 
 /*** Virtual functions ***/
-double default_model::iClamp(double x){ // Derived class iClamp() should be called, never base class function
-    cout << "****ERROR**** default_model iClamp called, function not defined in derived class" << endl;
-    return 0;
+
+// Derived class iClamp() should be called, never base class function
+double default_model::iClamp(double x){ 
+	cout << "****ERROR**** default_model iClamp called, function not defined in derived class" << endl;
+	return 0;
 }
 
-double default_model::vClamp(double x){ // Derived class vClamp() should be called, never base class function
-    cout << "****ERROR**** default_model vClamp called, function not defined in derived class" << endl;
-    return 0;
+// Derived class vClamp() should be called, never base class function
+double default_model::vClamp(double x){ 
+	cout << "****ERROR**** default_model vClamp called, function not defined in derived class" << endl;
+	return 0;
 }
 
-void default_model::reset(){ // Derived class reset() should be called, never base class function
-    cout << "****ERROR**** default_model reset called, function not defined in derived class" << endl;
+// Derived class reset() should be called, never base class function
+void default_model::reset(){ 
+	cout << "****ERROR**** default_model reset called, function not defined in derived class" << endl;
 }
 
-int default_model::reset(double x){ // Derived class reset(double) should be called, never base class function
-    cout << "****ERROR**** default_model reset(double) called, function not defined in derived class" << endl;
+// Derived class reset(double) should be called, never base class function
+int default_model::reset(double x){ 
+	cout << "****ERROR**** default_model reset(double) called, function not defined in derived class" << endl;
 }
 
-void default_model::reset(double *x){ // Derived class reset(*double) should be called, never base class function
-    cout << "****ERROR**** default_model reset(*double) called, function not defined in derived class" << endl;
+// Derived class reset(*double) should be called, never base class function
+void default_model::reset(double *x){ 
+	cout << "****ERROR**** default_model reset(*double) called, function not defined in derived class" << endl;
 }
 
-double default_model::current(int x){ // Derived class current() should be called, never base class function
-    cout << "****ERROR**** default_model current() called, function not defined in derived class" << endl;
+// Derived class current() should be called, never base class function
+double default_model::current(int x){ 
+	cout << "****ERROR**** default_model current() called, function not defined in derived class" << endl;
 }
 
-double default_model::concentration(int x){ // Derived class concentration() should be called, never base class function
-    cout << "****ERROR**** default_model concentratoin() called, function not defined in derived class" << endl;
+// Derived class concentration() should be called, never base class function
+double default_model::concentration(int x){ 
+	cout << "****ERROR**** default_model concentratoin() called, function not defined in derived class" << endl;
 }
 
-double default_model::param(int x){ // Derived class param() should be called, never base class function
-    cout << "****ERROR**** default_model param(int) called, function not defined in derived class" << endl;
+// Derived class param() should be called, never base class function
+double default_model::param(int x){ 
+	cout << "****ERROR**** default_model param(int) called, function not defined in derived class" << endl;
 }
 
-double default_model::param(string x){ // Derived class param() should be called, never base class function
-    cout << "****ERROR**** default_model param(string) called, function not defined in derived class" << endl;
+// Derived class param() should be called, never base class function
+double default_model::param(string x){ 
+	cout << "****ERROR**** default_model param(string) called, function not defined in derived class" << endl;
 }
 
-void default_model::setParam(int x, double y){ // Derived class setParam() should be called, never base class function
-    cout << "****ERROR**** default_model setParam() called, function not defined in derived class" << endl;
+// Derived class setParam() should be called, never base class function
+void default_model::setParam(int x, double y){ 
+	cout << "****ERROR**** default_model setParam() called, function not defined in derived class" << endl;
 }
 
-QStringList default_model::paramList( void ){ // Derived class setParam() should be called, never base class function
-    cout << "****ERROR**** default_model paramList() called, function not defined in derived class" << endl;
+// Derived class setParam() should be called, never base class function
+QStringList default_model::paramList( void ){ 
+	cout << "****ERROR**** default_model paramList() called, function not defined in derived class" << endl;
 }
